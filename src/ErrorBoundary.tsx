a4714e790b2d6ae880148c21b082af58ef9b1cf1
arestus <arestus@gmail.com>
2024-04-10 15:46:22 +0200

    adds ErrorBoundary component

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,23 @@
+import { Component, ErrorInfo, PropsWithChildren } from "react";
+
+export class ErrorBoundary extends Component<PropsWithChildren<any>> {
+  state: { hasError: boolean };
+  constructor(props: { children: JSX.Element }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    alert(error);
+  }
+
+  static getDerivedStateFromError(error: Error): { hasError: boolean } {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) return <h2>Something went wrong.</h2>;
+
+    return this.props.children;
+  }
+}
