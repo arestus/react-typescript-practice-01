140c40103ff148d72652193dcd8fcb73a770c8fd
arestus <arestus@gmail.com>
2024-04-11 21:36:50 +0200

    state update

--- src/reducers/movies.ts
+++ src/reducers/movies.ts
@@ -13,49 +13,64 @@ export interface Movie {
 interface MovieState {
   top: Movie[];
   loading: boolean;
+  page: number;
+  hasMorePages: boolean;
 }
 
 const initialState = {
   top: [],
   loading: false,
+  page: 0,
+  hasMorePages: true,
 };
 
-const moviesLoaded = (movies: Movie[]) => ({
+const moviesLoaded = (movies: Movie[], page: number, hasMorePages: boolean) => ({
   type: "movies/loaded",
-  payload: movies,
+  payload: { movies, page, hasMorePages },
 });
 
 const moviesLoading = () => ({
   type: "movies/loading",
 });
 
-export function fetchMovies(): AppThunk<Promise<void>> {
+export function fetchNextPage(): AppThunk<Promise<void>> {
   return async (dispatch, getState) => {
+    const nextPage = getState().movies.page + 1;
+    dispatch(fetchPage(nextPage));
+  };
+}
+
+function fetchPage(page: number): AppThunk<Promise<void>> {
+  return async (dispatch) => {
     dispatch(moviesLoading());
 
     const config = await client.getConfiguration();
     const imageUrl = config.images.base_url;
-    const results = await client.getNowPlaying();
+    const nowPlaying = await client.getNowPlaying(page);
 
-    const mappedResults: Movie[] = results.map((movie) => ({
+    const mappedResults: Movie[] = nowPlaying.results.map((movie) => ({
       id: movie.id,
       title: movie.title,
       overview: movie.overview,
       popularity: movie.popularity,
-      image: movie.backdrop_path
-        ? `${imageUrl}w780${movie.backdrop_path}`
-        : undefined,
+      image: movie.backdrop_path ? `${imageUrl}w780${movie.backdrop_path}` : undefined,
     }));
 
-    dispatch(moviesLoaded(mappedResults));
+    const hasMorePage = nowPlaying.page < nowPlaying.totalPages;
+
+    dispatch(moviesLoaded(mappedResults, page, hasMorePage));
   };
 }
-
 const moviesReducer = createReducer<MovieState>(initialState, {
-  "movies/loaded": (state, action: ActionWithPayload<Movie[]>) => {
+  "movies/loaded": (
+    state,
+    action: ActionWithPayload<{ movies: Movie[]; page: number; hasMorePages: boolean }>
+  ) => {
     return {
       ...state,
-      top: action.payload,
+      top: [...state.top, ...action.payload.movies],
+      page: action.payload.page,
+      hasMorePages: action.payload.hasMorePages,
       loading: false,
     };
   },
