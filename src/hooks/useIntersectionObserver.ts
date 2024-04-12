85ad57810a3e73b39bdb0a4e93a17602184da835
arestus <arestus@gmail.com>
2024-04-12 18:33:25 +0200

    add custom hook for request during scrolling

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
+interface Options {
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number | number[];
+}
+
+type HookReturnType = [React.MutableRefObject<null>, IntersectionObserverEntry?];
+
+export function useIntersectionObserver(options: Options = {}): HookReturnType {
+  const { threshold = 1.0, root = null, rootMargin = "0px" } = options;
+  const targetRef = useRef(null);
+
+  const [entry, setEntry] = useState<IntersectionObserverEntry>();
+
+  function callbackFn(entries: IntersectionObserverEntry[]) {
+    const [entry] = entries;
+    setEntry(entry);
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFn, { threshold, root, rootMargin });
+    const currentRef = targetRef.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return function () {
+      if (currentRef) {
+        observer.disconnect();
+      }
+    };
+  }, [root, rootMargin, threshold]);
+
+  return [targetRef, entry];
+}
